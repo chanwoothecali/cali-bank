b12aec393a1a6dda04303446cb642d9fa46ad9e0
jungcali94 <jungcali94@gmail.com>
2023-06-26 16:04:32 +0900

    first commit

---
+++ src/main/java/com/cali/calibank/CalibankApplication.java
@@ -0,0 +1,13 @@
+package com.cali.calibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalibankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cali/calibank/CalibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cali.calibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
