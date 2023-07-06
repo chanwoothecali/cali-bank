727496e2b7a0e86142c432627395c0d2406c7117
chanwoothecali <jungcali94@gmail.com>
2023-07-06 15:27:02 +0900

    JPQL native query parameter 오류 수정

--- src/main/java/com/cali/calibank/user/repository/TokenRepository.java
+++ src/main/java/com/cali/calibank/user/repository/TokenRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
@@ -13,7 +14,7 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
       on t.user.id = u.id\s
       where u.id = :id and (t.expired = false or t.revoked = false)\s
       """)
-    List<Token> findAllValidTokenByUser(Long id);
+    List<Token> findAllValidTokenByUser(@Param("id") Long id);
 
     Optional<Token> findByToken(String token);
 }
